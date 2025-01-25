200799ecd388b60206fc769a374a1d0b1c470b01
Bipin87 <sahubipin484@gmail.com>
2025-01-25 09:47:09 +0530

    Single level inheritance is done

---
+++ Day-05/Single-Inheritance/LibraryManagement/Book.java
@@ -0,0 +1,21 @@
+package LibraryManagement;
+// Super class
+
+public class Book{
+    // Attributes
+    private String title;
+    private int publicationYear;
+
+    // Constructor
+    public Book(String title, int publicationYear){
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+
+    // Method to display book detail
+    public void displayInfo(){
+        System.out.println("Book title: " + title + "\n Book publication year: " + publicationYear);
+        
+    }
+
+}
---
+++ Day-05/Single-Inheritance/SmartHomeDevices/Device.java
@@ -0,0 +1,20 @@
+package SmartHomeDevices;
+// Base class
+public class Device {
+    // Attributes
+    private int deviceId;
+    private String status;
+
+    // Constructor
+    public Device(int deviceId, String status){
+        this.deviceId = deviceId;
+        this.status = status;
+    }
+
+    // Method to display Device details
+    public void displayStatus(){
+        System.out.println("Device id: "+ deviceId + "\n device status: " + status);
+
+    }
+    
+}
